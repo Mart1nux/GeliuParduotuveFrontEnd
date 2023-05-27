d09e3e7764b065d65fb3bcc5141d812e7efdcb97
Martinas Mačernius <76516162+Mart1nux@users.noreply.github.com>
2023-05-27 20:55:17 +0300

    Update

---
+++ GeliuParduotuveFrontEnd/Models/CartItem.cs
@@ -0,0 +1,18 @@
+﻿namespace GeliuParduotuveFrontEnd.Models
+{
+    public class CartItem
+    {
+        public int CustomerId { get; set; }
+
+        public int ItemId { get; set; }
+
+        public int Amount { get; set; }
+
+        public CartItem(int customerId, int itemId, int amount)
+        {
+            CustomerId = customerId;
+            ItemId = itemId;
+            Amount = amount;
+        }
+    }
+}
